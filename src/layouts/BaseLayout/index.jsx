ebaa0a97221b389559e9063dde9c6239d7278aca
김기현 <remiacial@gmail.com>
2025-04-21 11:19:53 +0900

    feature: 로그인 모달 생성

--- src/layouts/BaseLayout/index.jsx
+++ src/layouts/BaseLayout/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { createPortal } from "react-dom";
 
 function BaseLayout() {
+  const [showModal, setShowModal] = React.useState(false);
+
+  function handleLoginClick() {
+    setShowModal(true);
+  }
+
+  function handleCancelClick() {
+    setShowModal(false);
+  }
+
   return (
     <div>
       <header>
         <div>Router App</div>
         <div>
-          <button>로그인</button>
+          <button onClick={handleLoginClick}>로그인</button>
         </div>
       </header>
 
@@ -17,12 +28,6 @@ function BaseLayout() {
             <ul>
               <li>
                 <a href="/">홈</a>
-              </li>
-              <li>
-                <a href="/about">소개</a>
-              </li>
-              <li>
-                <a href="/contact">연락처</a>
               </li>
             </ul>
           </nav>
@@ -34,8 +39,34 @@ function BaseLayout() {
       </div>
 
       <footer>
-        <p>© 2024 Router App. All rights reserved.</p>
+        <p></p>
       </footer>
+
+      {showModal &&
+        createPortal(
+          <div
+            onClick={function (e) {
+              if (e.target === e.currentTarget) {
+                setShowModal(false);
+              }
+            }}
+          >
+            <div>
+              <h2>로그인</h2>
+              <div>
+                <label>이름</label>
+                <input type="text" placeholder="아이디를 입력하세요" />
+              </div>
+              <div>
+                <button onClick={handleCancelClick}>취소</button>
+                <button>로그인</button>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
     </div>
   );
 }
+
+export default BaseLayout;
