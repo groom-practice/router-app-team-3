2064d63311af0acc29b66256c3c82e850fb52f64
gaeunchoi <gaeun1884@naver.com>
2025-04-21 13:45:44 +0900

    fix: Link 경로 수정 및 Modal 오류 수정

--- src/pages/PostDetail/index.jsx
+++ src/pages/PostDetail/index.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getPostById } from "../../apis/posts";
 import { createPortal } from "react-dom";
+import PortalModalContainer from "../../components/PortalModalContainer";
 
-// const POST = { id: 1, title: "dumpTest", body: "asdfasdfasdfasdfasdf" };
 export default function PostDetail() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
@@ -37,26 +37,28 @@ export default function PostDetail() {
   if (!post) return <div className="post-detail-container">로딩중입니다</div>;
   return (
     <div className="post-detail-container">
-      <h1>현재 포스트의 포스트 아이디는 {id}입니다.</h1>
+      <h1>Post Id: {id}</h1>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
 
       <div className="post-detail-buttons">
-        <Link to={`posts/${id}/edit`}>Edit</Link>
+        <Link to={`/posts/${id}/edit`}>Edit</Link>
         <button onClick={handleFavoriteBtn}>즐겨찾기</button>
       </div>
 
-      {
-        (showModal &&
-          createPortal(
-            <PortalModalContainer>
-              <h3>아이디:{id}인 게시글을 즐겨찾기에 추가</h3>
-              <button onClick={confirmFavorite}>확인</button>
-              <button onClick={cancelFavorite}>취소</button>
-            </PortalModalContainer>
-          ),
-        document.body)
-      }
+      {showModal &&
+        createPortal(
+          <PortalModalContainer>
+            <div className="favorite-modal">
+              <h3>아이디 = {id}인 게시글을 즐겨찾기에 추가?</h3>
+              <div className="favorite-modal-buttons">
+                <button onClick={confirmFavorite}>확인</button>
+                <button onClick={cancelFavorite}>취소</button>
+              </div>
+            </div>
+          </PortalModalContainer>,
+          document.body
+        )}
     </div>
   );
 }
