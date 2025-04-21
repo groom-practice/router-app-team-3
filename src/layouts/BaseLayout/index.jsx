f8626483d95ad78c50bf66156ef49a41f6adfe00
김기현 <remiacial@gmail.com>
2025-04-21 17:38:46 +0900

    feature: 기본 레이아웃 구성

--- src/layouts/BaseLayout/index.jsx
+++ src/layouts/BaseLayout/index.jsx
@@ -1,72 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import { createPortal } from "react-dom";
+import { Outlet, Link } from "react-router-dom";
+import "./index.css";
 
-function BaseLayout() {
-  const [showModal, setShowModal] = React.useState(false);
-
-  function handleLoginClick() {
-    setShowModal(true);
-  }
-
-  function handleCancelClick() {
-    setShowModal(false);
-  }
-
+const BaseLayout = () => {
   return (
-    <div>
+    <main>
       <header>
-        <div>Router App</div>
-        <div>
-          <button onClick={handleLoginClick}>로그인</button>
-        </div>
+        <h1>POST PROJECT</h1>
       </header>
-
-      <div>
-        <aside>
-          <nav>
-            <ul>
-              <li>
-                <a href="/">홈</a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-
-        <main>
+      <div className="mainContent">
+        <div className="leftSideBar">
+          <Link to="/">HOME</Link>
+          <Link to="/posts">POSTS</Link>
+          <button>BACK</button>
+        </div>
+        <div className="content">
           <Outlet />
-        </main>
+        </div>
       </div>
-
-      <footer>
-        <p></p>
-      </footer>
-
-      {showModal &&
-        createPortal(
-          <div
-            onClick={function (e) {
-              if (e.target === e.currentTarget) {
-                setShowModal(false);
-              }
-            }}
-          >
-            <div>
-              <h2>로그인</h2>
-              <div>
-                <label>이름</label>
-                <input type="text" placeholder="아이디를 입력하세요" />
-              </div>
-              <div>
-                <button onClick={handleCancelClick}>취소</button>
-                <button>로그인</button>
-              </div>
-            </div>
-          </div>,
-          document.body
-        )}
-    </div>
+    </main>
   );
-}
+};
 
 export default BaseLayout;
