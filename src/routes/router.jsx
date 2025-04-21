82b658ede7d322425e0f13b446779ae13c708b5d
gaeunchoi <gaeun1884@naver.com>
2025-04-21 10:51:43 +0900

    feat: 페이지 라우팅 설정

---
+++ src/routes/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import BaseLayout from "../layouts/BaseLayout";
+import Home from "../pages/Home";
+import PostList from "../pages/PostList";
+import PostDetail from "../pages/PostDetail";
+import EditPost from "../pages/EditPost";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BaseLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts", element: <PostList /> },
+      { path: "posts/:id", element: <PostDetail /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+    ],
+  },
+]);
+
+export default router;
